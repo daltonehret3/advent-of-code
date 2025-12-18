9552b13afcb81ff4a1f9a66c08a16f320c6d9549
Dalton Ehret <ehretd@pella.com>
2025-12-18 10:11:11 -0600

    feat(2025.2.1): Started On Part One (Much More to Go)

--- 2025/2/main.go
+++ 2025/2/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func partOne(input string) int {
+	total := 0
+	splitInput := strings.Split(input, "-")
+
+	leadingZeros := 0
+
+	for _, split := range splitInput {
+		if strings.HasPrefix(split, "0") {
+			total, _ = strconv.Atoi(split)	
+		}
+	}
+	
+	fmt.Println("Split Input", splitInput)
+
+	fmt.Println("Total", total)
+
+
+	return leadingZeros
+}
 
 func main() {
-	fmt.Println("Day 2")
+	total := 0
+
+	invalidId := partOne("01234-4351")
+
+	total += invalidId
+
+	fmt.Println("InvalidId", invalidId)
 }
