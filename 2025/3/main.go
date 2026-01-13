cdcd2a03ee838cbfa755e693b2525f6b0e2487a4
Dalton Ehret <ehretd@pella.com>
2026-01-13 13:40:12 -0600

    feat(2025.3.1): Solution to the First Step

--- 2025/3/main.go
+++ 2025/3/main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func step1(input string) int {
+	final := 0
+	maxNumber := 0
+	secondMax := 0
+	position := 0
+
+	for i, character := range input {
+		val := string(character)
+
+		number, _ := strconv.Atoi(val)
+
+		if (number > maxNumber) && i != len(input) -1 {
+			position = i
+			maxNumber = number	
+		}
+
+	}
+
+	for j := (position + 1); j < len(input); j++ {
+		secondVal := string(input[j])
+		secondNum, _ := strconv.Atoi(secondVal)
+
+		if secondNum > secondMax && j != position {
+			secondMax = secondNum
+		}
+	}
+
+	finalString := strconv.Itoa(maxNumber) + strconv.Itoa(secondMax)
+
+	final, _ = strconv.Atoi(finalString)
+
+	return final
+}
 
 func main() {
-	fmt.Println("Day 3")
+	powerBanks := []string{}
+	totalPower := 0
+
+	file, _ := os.Open("2025/3/input.txt")
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		powerBanks = append(powerBanks, scanner.Text())
+	}
+
+	for _, bank := range powerBanks {
+		power := step1(bank)
+
+		totalPower += power
+	}
+
+	fmt.Println("Final Power: ", totalPower)
 }
